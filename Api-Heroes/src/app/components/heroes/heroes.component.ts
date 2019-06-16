b58f8965ad3ce29f045467bcbf581011959c25d1
jvillamizar <jvillamizar@pratechgroup.com>
2019-06-16 02:10:28 -0500

    funcionalidad terminada iniciando estilos responsives

--- Api-Heroes/src/app/components/heroes/heroes.component.ts
+++ Api-Heroes/src/app/components/heroes/heroes.component.ts
@@ -1,26 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../service/heroes.service';
+import { Heroe } from '../model/heroe';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit() {
     this.listHeroes();
-    
   }
+
+  list = true;
+  detail = false;
 
   listHeroes() {
     this.heroesService.getHeroes()
       .subscribe(res => {
-        console.log(res);       
         this.heroesService.heroes = res as {};
       });
+
+  }
+  mostrarHeroe(heroe: Heroe, event) {
+    console.log('evento', event);
+    // event.stopPropagation();
+    this.modal = heroe;
+    this.list = false;
+    this.detail = true;
+    
+
   }
 
+  modal: Heroe = new Heroe;
+
+  votoLike(id) {
+    
+    id = id + ' Likes';
+    console.log(id);
+    if(!localStorage[id]){
+      localStorage.setItem(id,'1');
+      console.log('GUARDADO => ',localStorage.getItem(id));
+    } else {
+      var likei = parseInt(localStorage.getItem(id));
+      likei++
+      var likes = likei.toString();
+      localStorage.setItem(id, likes)
+      console.log('GUARDADO => ',localStorage.getItem(id));
+    }
+  }
+
+  votoDislike(id) {
+    id = id + ' Dislikes';
+    console.log(id);
+    if(!localStorage[id]){
+      localStorage.setItem(id,'1');
+      console.log('GUARDADO => ',localStorage.getItem(id));
+    } else {
+      var likei = parseInt(localStorage.getItem(id));
+      likei++
+      var likes = likei.toString();
+      localStorage.setItem(id, likes)
+      console.log('GUARDADO => ',localStorage.getItem(id));
+    }
+  }
+
+  return(){
+    this.list = true;
+    this.detail = false;
+  }
 }
---
+++ Api-Heroes/src/app/components/model/heroe.ts
@@ -0,0 +1,15 @@
+export class Heroe {
+    constructor(_id='', name='', picture='', publisher='', info=''){
+        this._id=_id,
+        this.name=name,
+        this.picture=picture,
+        this.publisher=publisher,
+        this.info=info
+    }
+    _id: string;
+    name: string;
+    picture: string
+    publisher:string;
+    info:string;
+}
+
